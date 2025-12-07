aba1908229ec4381577e072846dacd68a7ff2b05
TheTureyProgramist <150546608+TheTureyProgramist@users.noreply.github.com>
2025-12-07 10:39:19 +0200

    /

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-function App() {
+import styled from 'styled-components';
+import { Component } from 'react';
+class App extends Component {
+  render() {
   return (
     <div className="App">
   
     </div>
   );
+}
 }
 export default App;
